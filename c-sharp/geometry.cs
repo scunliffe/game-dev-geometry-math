74cb1cde084fe1cd671a5cafc5e80cad865230bd
Stephen Cunliffe <stephen.cunliffe@gmail.com>
2023-06-28 20:46:33 -0400

    Update geometry.cs

--- c-sharp/geometry.cs
+++ c-sharp/geometry.cs
@@ -73,7 +73,6 @@ public static float distance(Vector3 point1, Vector3 point2){
 	return Vector3.Distance(point1, point2);
 }
 
-
 public static bool isPointOnLine3D(Vector3 point, Vector3 lineStart, Vector3 lineEnd){
 	float epsilon = 0.0001f;
 	float dx = lineEnd.x - lineStart.x;
@@ -86,4 +85,44 @@ public static bool isPointOnLine3D(Vector3 point, Vector3 lineStart, Vector3 lin
 	float t1 = (point.x - lineStart.x) * dx + (point.y - lineStart.y) * dy + (point.z - lineStart.z) * dz;
 	float t2 = (lineEnd.x - point.x) * dx + (lineEnd.y - point.y) * dy + (lineEnd.z - point.z) * dz;
 	return Mathf.Abs(t1 + t2 - len) < epsilon;
+}
+
+public static Vector3 getNormalOfPlane(Vector3 point1, Vector3 point2, Vector3 point3){
+	Vector3 side1 = point2 - point1;
+	Vector3 side2 = point3 - point1;
+	return Vector3.Cross(side1, side2).normalized;
+}
+
+public static Vector2 getLineIntersection(Vector2 line1Start, Vector2 line1End, Vector2 line2Start, Vector2 line2End){
+	Vector2 intersection = null;
+
+	Vector2 dir1 = line1End - line1Start;
+	Vector2 dir2 = line2End - line2Start;
+
+	float a1 = -dir1.y;
+	float b1 = +dir1.x;
+	float d1 = -(a1 * line1Start.x + b1 * line1Start.y);
+
+	float a2 = -dir2.y;
+	float b2 = +dir2.x;
+	float d2 = -(a2 * line2Start.x + b2 * line2Start.y);
+
+	float seg1_line2_start = a2 * line1Start.x + b2 * line1Start.y + d2;
+	float seg1_line2_end = a2 * line1End.x + b2 * line1End.y + d2;
+
+	float seg2_line1_start = a1 * line2Start.x + b1 * line2Start.y + d1;
+	float seg2_line1_end = a1 * line2End.x + b1 * line2End.y + d1;
+
+	// If segments intersect, calculate intersection point
+	if((seg1_line2_start > 0 && seg1_line2_end < 0) || (seg1_line2_start < 0 && seg1_line2_end > 0)){
+		if((seg2_line1_start > 0 && seg2_line1_end < 0) || (seg2_line1_start < 0 && seg2_line1_end > 0)){
+			float u = seg1_line2_start / (seg1_line2_start - seg1_line2_end);
+			intersection = line1Start + u * dir1;
+		}
+	}
+	return intersection;
+}
+
+public static bool doLinesIntersect(Vector2 line1Start, Vector2 line1End, Vector2 line2Start, Vector2 line2End){
+	return (getLineIntersection(line1Start, line1End, line2Start, line2End) != null);
 }
